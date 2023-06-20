9a86bf0ff3094b7f57c980fbe6a21569702535b3
Wolves <cstingl@protonmail.com>
2023-06-20 13:37:07 -0400

    Shuttle Launchpad #1 Complete

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,23 @@
 use axum::{response::IntoResponse, routing::get, Router};
-use std::net::SocketAddr;
 
 async fn hello_world() -> impl IntoResponse {
     "Hello world"
 }
 
-#[tokio::main]
-async fn main() {
+// #[tokio::main]
+// async fn main() {
+//     let app = Router::new().route("/", get(hello_world));
+//
+//     let addr = SocketAddr::new([0, 0, 0, 0].into(), 3000);
+//
+//     axum::Server::bind(&addr)
+//         .serve(app.into_make_service())
+//         .await
+//         .unwrap();
+// }
+//
+#[shuttle_runtime::main]
+async fn shuttle() -> shuttle_axum::ShuttleAxum {
     let app = Router::new().route("/", get(hello_world));
-
-    let addr = SocketAddr::new([0, 0, 0, 0].into(), 3000);
-
-    axum::Server::bind(&addr)
-        .serve(app.into_make_service())
-        .await
-        .unwrap();
+    Ok(app.into())
 }
